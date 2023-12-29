ac231e640a1de9337015d744e5c4ac531f5ae8a2
Nathalie Chang <nathalie.ch.chang@gmail.com>
2023-12-29 03:23:26 -0800

    added carousel auto rotation

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,26 +1,12 @@
-// This script.js is modified from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
+//every 3 secs, force a click on input "carousel-dot"
+const radioInputs = document.querySelectorAll(".carousel-dot");
+let activeCarouselIndex = 0;
 
-let slideIndex = 0;
-showSlides();
-console.log(showSlides());
+function autoShowSlides() {
+  activeCarouselIndex = (activeCarouselIndex + 1) % 3;
+  radioInputs[activeCarouselIndex].click();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("carousel__item");
-  let dots = document.getElementsByClassName("carousel-dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" checked", "");
-  }
-  // slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " checked";
-  console.log(dots.className);
-
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(autoShowSlides, 3000); // Change image every 3 seconds
 }
+
+setTimeout(autoShowSlides, 3000);
