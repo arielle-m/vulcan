fe17e7a223deb1b8d2408444dbbd9b94e7807dbc
Nathalie Chang <nathalie.ch.chang@gmail.com>
2023-12-30 21:38:52 -0800

    modify banner section and carousel autoslides

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,12 +1,34 @@
 //every 3 secs, force a click on input "carousel-dot"
 const radioInputs = document.querySelectorAll(".carousel-dot");
+const labels = document.querySelectorAll(".carousel__nav label");
+
+const CAROUSEL_DELAY = 3000;
+
 let activeCarouselIndex = 0;
+let timeoutID;
+setTimeout(autoShowSlides, CAROUSEL_DELAY);
 
 function autoShowSlides() {
-  activeCarouselIndex = (activeCarouselIndex + 1) % 3;
+  activeCarouselIndex++;
+  console.log(activeCarouselIndex);
   radioInputs[activeCarouselIndex].click();
 
-  setTimeout(autoShowSlides, 3000); // Change image every 3 seconds
+  if (activeCarouselIndex === radioInputs.length - 1) {
+    activeCarouselIndex = -1;
+  }
+
+  // Change image every 3 seconds
+  timeoutID = setTimeout(autoShowSlides, CAROUSEL_DELAY);
 }
 
-setTimeout(autoShowSlides, 3000);
+for (let i = 0; i < labels.length; i++) {
+  labels[i].addEventListener("click", function () {
+    clearTimeout(timeoutID);
+    activeCarouselIndex = i;
+    if (activeCarouselIndex === radioInputs.length - 1) {
+      activeCarouselIndex = -1;
+    }
+
+    timeoutID = setTimeout(autoShowSlides, CAROUSEL_DELAY);
+  });
+}
